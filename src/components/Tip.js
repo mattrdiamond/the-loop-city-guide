258aabd8254a18d89b6bd69c5549f446a1b9c047
Matt Diamond <mattdiamond80@gmail.com>
2020-12-21 22:06:55 -0600

    Update tip section with fake avatars since no longer included on FourSquare

--- src/components/Tip.js
+++ src/components/Tip.js
@@ -1,15 +1,44 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
 
 const Tip = ({ venue, formatDate }) => {
   const users = venue.tips.groups[0].items;
+  const [avatars, setAvatars] = useState(null);
+
+  useEffect(() => {
+    let isMounted = true;
+    (async () => {
+      const data = await fetchAvatars(users.length);
+      if (isMounted) {
+        const avatars = data.results.map(({ picture }) => picture.thumbnail);
+        setAvatars(avatars);
+      }
+
+      return () => (isMounted = false);
+    })();
+  }, []);
+
+  const fetchAvatars = (num) => {
+    const avatars = axios
+      .get(`https://randomuser.me/api/1.3?results=${num}&inc=picture`)
+      .then((res) => {
+        return res.data;
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+    return avatars;
+  };
 
   return users.map((user, index) => (
     <li key={venue.id + '_tip_' + index} className="tip">
-      <img
-        className="tip-photo"
-        src={user.user.photo.prefix + '50x50' + user.user.photo.suffix}
-        alt={'a photo of ' + user.user.firstName}
-      />
+      {avatars && (
+        <img
+          className="tip-photo"
+          src={avatars[index]}
+          alt={'a photo of ' + user.user.firstName}
+        />
+      )}
       <div className="tip-text">
         <p className="tip-quote">&ldquo;{user.text}&rdquo;</p>
         <span className="user-name">{user.user.firstName}</span>
